76a228ee9a67f855746aedb373c04f0c2bad6b4e
Cameron Woodbury <cam@hobu.co>
2025-05-20 15:12:27 -0500

    Remove previously added feature
    Still just testing changelog from PRs

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,3 @@ import { init } from '../src'
 test('Hello, World!', () => {
   expect(init()).toBe('Hello, World!')
 })
-
-test('Hello, Alice!', () => {
-  expect(init.greet('Alice')).toBe('Hello, Alice!')
-})
