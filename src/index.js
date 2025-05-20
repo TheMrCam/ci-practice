649fe50c9263799a59e32c7464fbb7c017ba3f36
Cameron Woodbury <63317483+TheMrCam@users.noreply.github.com>
2025-05-20 15:06:35 -0500

    Add feature: init.greet function (#4)
    Trying out the changelog generation with multiple labels on a single PR

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 export function init() {
   return 'Hello, World!'
 }
+init.greet = function (name) {
+  return `Hello, ${name}!`
+}
 
 export default init
--- test/index.test.js
+++ test/index.test.js
@@ -3,3 +3,7 @@ import { init } from '../src'
 test('Hello, World!', () => {
   expect(init()).toBe('Hello, World!')
 })
+
+test('Hello, Alice!', () => {
+  expect(init.greet('Alice')).toBe('Hello, Alice!')
+})
