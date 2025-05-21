4e7664376f936c69953f44e4e6ca26870b30b7c4
Cameron Woodbury <63317483+TheMrCam@users.noreply.github.com>
2025-05-20 22:00:07 -0500

    Add greet function and tests (#7)
    Added greet() with tests, and a test to ensure init() is the default
    export.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ export function init() {
   return 'Hello, World!'
 }
 
+export function greet(name) {
+  return `Hello, ${name}!`
+}
+
 export default init
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,13 @@
-import { init } from '../src'
+import Default, { init, greet } from '../src'
 
 test('Hello, World!', () => {
   expect(init()).toBe('Hello, World!')
+})
+
+test.each(['Alice', 'Bob', 'Cameron'])('Hello, %s!', (name) => {
+  expect(greet(name)).toBe(`Hello, ${name}!`)
+})
+
+test('default export', () => {
+  expect(Default()).toBe(init())
 })
